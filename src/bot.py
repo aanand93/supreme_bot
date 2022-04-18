7724791f0cec6156475b3734c3d8298ebde3a6b0
aanand93 <77586218+aanand93@users.noreply.github.com>
2022-04-18 13:36:57 -0400

    will search for a specific item and post the name and ID that matches the Item

--- src/bot.py
+++ src/bot.py
@@ -2,11 +2,18 @@ import requests
 import json
 
 
-def find_item(item):
+def find_item(name):
     url = 'https://www.supremenewyork.com/mobile_stock.json'
     html = requests.get(url=url)
     output = json.loads(html.text)
-    print(output['products_and_categories'])
+
+    for category in output['products_and_categories']:
+        for item in output['products_and_categories'][category]:
+            if name in item['name']:
+                print(item['name'])
+                print(item['id'])
+                return item['id']
 
 
-find_item(None)
+# Type the item name inside find the exact item
+find_item('Cargo Pant')
